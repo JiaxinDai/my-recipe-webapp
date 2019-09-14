bb02a07e5f4ebf85e7f6d2f86d40579c7edeefb0
JiaxinDai <50279363+JiaxinDai@users.noreply.github.com>
2019-09-14 10:33:12 -0400

    Adding more stuff to apiController

--- recipeshare-application-jsp-servlet-jdbc-mysql/src/recipewebapp/controllers/RecipeApiController.java
+++ recipeshare-application-jsp-servlet-jdbc-mysql/src/recipewebapp/controllers/RecipeApiController.java
@@ -1,37 +1,220 @@
-import com.essalah.recipescraper.services.AllRecipeService;
-
-import org.springframework.ui.Model;
-
 import org.springframework.web.bind.annotation.*;
 
+import xyz.michaelsmith.cs4550.project.common.api.exception.ResourceNotFoundException;
+
+import xyz.michaelsmith.cs4550.project.recipe.dto.RecipeCommentDto;
+
+import xyz.michaelsmith.cs4550.project.recipe.dto.RecipeDto;
+
+import xyz.michaelsmith.cs4550.project.recipe.service.RecipeService;
 
 
 
-import java.io.IOException;
+import java.util.List;
 
+
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 
 
 @RestController
 
+@RequestMapping("/api/recipe")
+
 public class RecipeApiController {
 
+    private final RecipeService recipeService;
 
 
 
-    @RequestMapping(method = RequestMethod.GET, value = "/fromurl")
+    public RecipeApiController(RecipeService recipeService) {
 
-    public Object getRecipeFromGivenUrl(@RequestParam("url") String url){
+        this.recipeService = recipeService;
+
+    }
+
+
+
+    @RequestMapping(path = {"", "/"},
+
+                    method = GET,
+
+                    produces = APPLICATION_JSON_VALUE)
+
+    public List<RecipeDto> getAll() {
+
+        return recipeService.getAll();
+
+    }
+
+
+
+    @RequestMapping(path = {"", "/"},
+
+                    method = GET,
+
+                    params = {"title"},
+
+                    produces = APPLICATION_JSON_VALUE)
+
+    public List<RecipeDto> searchByTitle(@RequestParam("title") String title) {
+
+        return recipeService.searchRecipesByTitle(title);
+
+    }
+
+
+
+    @RequestMapping(path = {"", "/"},
+
+                    method = GET,
+
+                    params = {"ingredient"},
+
+                    produces = APPLICATION_JSON_VALUE)
+
+    public List<RecipeDto> searchByIngredient(@RequestParam("ingredient") Long ingredient) {
 
         try {
 
-            return AllRecipeService.getFromUrl(url);
+            return recipeService.searchRecipesByIngredient(ingredient);
 
-        } catch (IOException e) {
+        } catch (IllegalArgumentException ex) {
 
-            e.printStackTrace();
+            throw new ResourceNotFoundException(ex.getMessage());
 
-            return null;
+        }
+
+    }
+
+
+
+    @RequestMapping(path = {"", "/"},
+
+            method = GET,
+
+            params = {"author"},
+
+            produces = APPLICATION_JSON_VALUE)
+
+    public List<RecipeDto> searchByAuthor(@RequestParam("author") Long userId) {
+
+        try {
+
+            return recipeService.searchRecipesByAuthor(userId);
+
+        } catch (IllegalArgumentException ex) {
+
+            throw new ResourceNotFoundException(ex.getMessage());
+
+        }
+
+    }
+
+
+
+    @RequestMapping(path = "/{recipeId}",
+
+                    method = GET,
+
+                    produces = APPLICATION_JSON_VALUE)
+
+    public RecipeDto getById(@PathVariable("recipeId") Long recipeId) {
+
+        try {
+
+            return recipeService.getRecipeById(recipeId);
+
+        } catch (IllegalArgumentException ex) {
+
+            throw new ResourceNotFoundException(ex.getMessage());
+
+        }
+
+    }
+
+
+
+    @RequestMapping(path = {"", "/"},
+
+                    method = POST,
+
+                    consumes = APPLICATION_JSON_VALUE,
+
+                    produces = APPLICATION_JSON_VALUE)
+
+    public RecipeDto createRecipe(@RequestBody RecipeDto recipeDto) {
+
+        try {
+
+            return recipeService.createRecipe(recipeDto);
+
+        } catch (IllegalArgumentException ex) {
+
+            throw new ResourceNotFoundException(ex.getMessage());
+
+        }
+
+    }
+
+
+
+    @RequestMapping(path = "/{recipeId}",
+
+                    method = PUT,
+
+                    consumes = APPLICATION_JSON_VALUE,
+
+                    produces = APPLICATION_JSON_VALUE)
+
+    public RecipeDto updateRecipe(@PathVariable("recipeId") Long recipeId, @RequestBody RecipeDto recipeDto) {
+
+        try {
+
+            return recipeService.updateRecipe(recipeId, recipeDto);
+
+        } catch (IllegalArgumentException ex) {
+
+            throw new ResourceNotFoundException(ex.getMessage());
+
+        }
+
+    }
+
+
+
+    @RequestMapping(path = "/{recipeId}",
+
+                    method = DELETE)
+
+    public void deleteRecipe(@PathVariable("recipeId") Long recipeId) {
+
+        recipeService.deleteRecipe(recipeId);
+
+    }
+
+
+
+    @RequestMapping(path = "/{recipeId}/comments",
+
+                    method = POST,
+
+                    consumes = APPLICATION_JSON_VALUE,
+
+                    produces = APPLICATION_JSON_VALUE)
+
+    public RecipeDto createComment(@PathVariable("recipeId") Long recipeId, @RequestBody RecipeCommentDto commentDto) {
+
+        try {
+
+            return recipeService.createComment(recipeId, commentDto);
+
+        } catch (IllegalArgumentException ex) {
+
+            throw new ResourceNotFoundException(ex.getMessage());
 
         }
 
