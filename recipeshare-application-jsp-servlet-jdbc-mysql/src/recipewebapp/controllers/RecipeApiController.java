68ab0a5712e04b478e3e6a152e829e5bc46713aa
joannalu97 <joannalu97@gmail.com>
2019-09-14 10:17:03 -0400

    Add apiController

---
+++ recipeshare-application-jsp-servlet-jdbc-mysql/src/recipewebapp/controllers/RecipeApiController.java
@@ -0,0 +1,40 @@
+import com.essalah.recipescraper.services.AllRecipeService;
+
+import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.*;
+
+
+
+
+import java.io.IOException;
+
+
+
+
+@RestController
+
+public class RecipeApiController {
+
+
+
+
+    @RequestMapping(method = RequestMethod.GET, value = "/fromurl")
+
+    public Object getRecipeFromGivenUrl(@RequestParam("url") String url){
+
+        try {
+
+            return AllRecipeService.getFromUrl(url);
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+            return null;
+
+        }
+
+    }
+
+}
