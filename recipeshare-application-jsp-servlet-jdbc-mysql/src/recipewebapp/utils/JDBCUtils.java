b639eda560f84c79f4c089a44aa2da5d5054a6d2
joannalu97 <joannalu97@gmail.com>
2019-09-14 09:47:03 -0400

    Update utils

--- recipeshare-application-jsp-servlet-jdbc-mysql/src/recipewebapp/utils/JDBCUtils.java
+++ recipeshare-application-jsp-servlet-jdbc-mysql/src/recipewebapp/utils/JDBCUtils.java
@@ -1,53 +1,299 @@
 package recipewebapp.utils;
 
 import java.sql.Connection;
-import java.sql.Date;
-import java.sql.DriverManager;
+
+import java.sql.PreparedStatement;
+
+import java.sql.ResultSet;
+
+import java.sql.ResultSetMetaData;
+
 import java.sql.SQLException;
-import java.time.LocalDate;
 
-public class JDBCUtils {
+import java.util.HashMap;
 
-	private static String jdbcURL = "jdbc:mysql://localhost:3306/demo";
-	private static String jdbcUsername = "root";
-	private static String jdbcPassword = ".%jrh:sZF85s";
+import java.util.Iterator;
 
-	public static Connection getConnection() {
-		Connection connection = null;
+import java.util.Map;
+
+
+
+import javax.sql.DataSource;
+
+
+
+public class JdbcUtils {
+
+
+
+	private DataSource dataSource;
+
+	private int fetchSize = 1000;
+
+
+
+	public JdbcUtils(DataSource dataSource) {
+
+		this.dataSource = dataSource;
+
+	}
+
+
+
+	public JdbcUtils(DataSource dataSource, int fetchSize) {
+
+		this.dataSource = dataSource;
+
+		this.fetchSize = fetchSize;
+
+	}
+
+
+
+	public void exec(final String sql, final Iterator<Object[]> paramsIterator, final Handler handler)
+
+			throws Exception {
+
+		Connection connection = dataSource.getConnection();
+
+		PreparedStatement statement = null;
+
+		ResultSet resultSet = null;
+
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return connection;
-	}
 
-	public static void printSQLException(SQLException ex) {
-		for (Throwable e : ex) {
-			if (e instanceof SQLException) {
-				e.printStackTrace(System.err);
-				System.err.println("SQLState: " + ((SQLException) e).getSQLState());
-				System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
-				System.err.println("Message: " + e.getMessage());
-				Throwable t = ex.getCause();
-				while (t != null) {
-					System.out.println("Cause: " + t);
-					t = t.getCause();
+			statement = connection.prepareStatement(sql);
+
+			statement.setFetchSize(fetchSize);
+
+
+
+			if (paramsIterator != null) {
+
+				int count = 0;
+
+				while (paramsIterator.hasNext()) {
+
+					Object[] params = paramsIterator.next();
+
+					for (int i = 0; i < params.length; i++) {
+
+						statement.setObject(i + 1, params[i]);
+
+					}
+
+					statement.addBatch();
+
+					if (count++ % fetchSize == 0) {
+
+						statement.executeBatch();
+
+					}
+
 				}
+
 			}
+
+			statement.executeBatch();
+
+			connection.commit();
+
+		} catch (SQLException e) {
+
+			connection.rollback();
+
+			throw e;
+
+		} finally {
+
+			if (resultSet != null) {
+
+				try {
+
+					resultSet.close();
+
+				} catch (SQLException e) {
+
+				}
+
+			}
+
+			if (statement != null) {
+
+				try {
+
+					statement.close();
+
+				} catch (SQLException e) {
+
+				}
+
+			}
+
+			if (connection != null) {
+
+				try {
+
+					connection.close();
+
+				} catch (SQLException e) {
+
+				}
+
+			}
+
 		}
+
+
+
 	}
 
-	public static Date getSQLDate(LocalDate date) {
-		return java.sql.Date.valueOf(date);
+
+
+	public void exec(final String sql, final Object[] params, final Handler handler) throws Exception {
+
+		Connection connection = dataSource.getConnection();
+
+		PreparedStatement statement = null;
+
+		ResultSet resultSet = null;
+
+		try {
+
+			statement = connection.prepareStatement(sql);
+
+			statement.setFetchSize(fetchSize);
+
+			if (params != null) {
+
+				for (int i = 0; i < params.length; i++) {
+
+					statement.setObject(i + 1, params[i]);
+
+				}
+
+			}
+
+
+
+			resultSet = statement.executeQuery();
+
+			handler.handle(resultSet);
+
+		} catch (SQLException e) {
+
+			throw e;
+
+		} finally {
+
+			if (resultSet != null) {
+
+				try {
+
+					resultSet.close();
+
+				} catch (SQLException e) {
+
+				}
+
+			}
+
+			if (statement != null) {
+
+				try {
+
+					statement.close();
+
+				} catch (SQLException e) {
+
+				}
+
+			}
+
+			if (connection != null) {
+
+				try {
+
+					connection.close();
+
+				} catch (SQLException e) {
+
+				}
+
+			}
+
+		}
+
+
+
 	}
 
-	public static LocalDate getUtilDate(Date sqlDate) {
-		return sqlDate.toLocalDate();
+
+
+	public void exec(final String sql, final Object[] params, final DataHandler handler) throws Exception {
+
+		exec(sql, params, new Handler() {
+
+
+
+			@Override
+
+			public void handle(ResultSet resultSet) throws Exception {
+
+				ResultSetMetaData rsm = resultSet.getMetaData();
+
+				final int columnCount = rsm.getColumnCount();
+
+				String[] columnName = new String[columnCount];
+
+				for (int i = 0; i < columnCount; i++) {
+
+					columnName[i] = rsm.getColumnName(i + 1);
+
+				}
+
+				int row = 1;
+
+				Map<String, Object> data = null;
+
+				while (resultSet.next()) {
+
+					data = new HashMap<>(columnCount);
+
+					for (int i = 0; i < columnCount; i++) {
+
+						data.put(columnName[i], resultSet.getObject(i + 1));
+
+					}
+
+					handler.handle(row++, data);
+
+				}
+
+			}
+
+
+
+		});
+
+
+
 	}
+
+
+
+	public static interface Handler {
+
+		public void handle(ResultSet resultSet) throws Exception;
+
+	}
+
+
+
+	public static interface DataHandler {
+
+		public void handle(int row, Map<String, Object> data) throws Exception;
+
+	}
+
 }
